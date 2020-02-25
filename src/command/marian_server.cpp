89fcbc2b35125944d516e26e4534d24f5d7127f3
Yifei Yu <yifyu@microsoft.com>
2020-02-25 22:03:44 +0800

    Replace server with a beast sync http server.
    Signed-off-by: Yifei Yu <yifyu@microsoft.com>

--- src/command/marian_server.cpp
+++ src/command/marian_server.cpp
@@ -1,61 +1,181 @@
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/config.hpp>
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
+
 #include "marian.h"
 #include "translator/beam_search.h"
 #include "translator/translator.h"
 #include "common/timer.h"
 #include "common/utils.h"
 
-#include "3rd_party/simple-websocket-server/server_ws.hpp"
+namespace beast = boost::beast;         // from <boost/beast.hpp>
+namespace http = beast::http;           // from <boost/beast/http.hpp>
+namespace net = boost::asio;            // from <boost/asio.hpp>
+using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
+using namespace marian;
 
-typedef SimpleWeb::SocketServer<SimpleWeb::WS> WSServer;
+//------------------------------------------------------------------------------
 
-int main(int argc, char **argv) {
-  using namespace marian;
+// This function produces an HTTP response for the given
+// request. The type of the response object depends on the
+// contents of the request, so the interface requires the
+// caller to pass a generic lambda for receiving the response.
+template<
+    class Body, class Allocator,
+    class Send>
+void
+handle_request(
+    std::shared_ptr<TranslateService<BeamSearch>> task,
+    http::request<Body, http::basic_fields<Allocator>>&& req,
+    Send&& send)
+{
+    std::cout << "tid: " << std::this_thread::get_id() << ", request path: " << req.target() << ", body: " << req.body() << std::endl;
 
-  // Initialize translation task
-  auto options = parseOptions(argc, argv, cli::mode::server, true);
-  auto task = New<TranslateService<BeamSearch>>(options);
-
-  // Initialize web server
-  WSServer server;
-  server.config.port = (short)options->get<size_t>("port", 8080);
-
-  auto &translate = server.endpoint["^/translate/?$"];
-
-  translate.on_message = [&task](Ptr<WSServer::Connection> connection,
-                                 Ptr<WSServer::Message> message) {
-    // Get input text
-    auto inputText = message->string();
-    auto sendStream = std::make_shared<WSServer::SendStream>();
-
-    // Translate
     timer::Timer timer;
-    auto outputText = task->run(inputText);
-    LOG(info, "Best translation: {}", outputText);
-    *sendStream << outputText << std::endl;
-    LOG(info, "Translation took: {:.5f}s", timer.elapsed());
+    auto outputText = task->run(req.body());
+    std::cout << "Best translation: " << outputText << std::endl;
+    std::cout << "Translation took: " << timer.elapsed() << "s" << std::endl;
 
-    // Send translation back
-    connection->send(sendStream, [](const SimpleWeb::error_code &ec) {
-      if(ec) {
-        LOG(error, "Error sending message: ({}) {}", ec.value(), ec.message());
-      }
-    });
-  };
+    http::response<http::string_body> res{http::status::ok, req.version()};
+    res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+    res.set(http::field::content_type, "text/plain");
+    res.keep_alive(req.keep_alive());
+    res.body() = std::string(outputText);
+    res.prepare_payload();
+    return send(std::move(res));
+}
 
-  // Error Codes for error code meanings
-  // http://www.boost.org/doc/libs/1_55_0/doc/html/boost_asio/reference.html
-  translate.on_error = [](Ptr<WSServer::Connection> connection,
-                          const SimpleWeb::error_code &ec) {
-    LOG(error, "Connection error: ({}) {}", ec.value(), ec.message());
-  };
+//------------------------------------------------------------------------------
 
-  // Start server thread
-  std::thread serverThread([&server]() {
-    LOG(info, "Server is listening on port {}", server.config.port);
-    server.start();
-  });
+// Report a failure
+void
+fail(beast::error_code ec, char const* what)
+{
+    std::cerr << what << ": " << ec.message() << "\n";
+}
 
-  serverThread.join();
+// This is the C++11 equivalent of a generic lambda.
+// The function object is used to send an HTTP message.
+template<class Stream>
+struct send_lambda
+{
+    Stream& stream_;
+    bool& close_;
+    beast::error_code& ec_;
 
-  return 0;
+    explicit
+    send_lambda(
+        Stream& stream,
+        bool& close,
+        beast::error_code& ec)
+        : stream_(stream)
+        , close_(close)
+        , ec_(ec)
+    {
+    }
+
+    template<bool isRequest, class Body, class Fields>
+    void
+    operator()(http::message<isRequest, Body, Fields>&& msg) const
+    {
+        // Determine if we should close the connection after
+        close_ = msg.need_eof();
+
+        // We need the serializer here because the serializer requires
+        // a non-const file_body, and the message oriented version of
+        // http::write only works with const messages.
+        http::serializer<isRequest, Body, Fields> sr{msg};
+        http::write(stream_, sr, ec_);
+    }
+};
+
+// Handles an HTTP server connection
+void
+do_session(
+    tcp::socket& socket,
+    std::shared_ptr<Options> const& options)
+{
+    auto task = New<TranslateService<BeamSearch>>(options);
+
+    bool close = false;
+    beast::error_code ec;
+
+    // This buffer is required to persist across reads
+    beast::flat_buffer buffer;
+
+    // This lambda is used to send messages
+    send_lambda<tcp::socket> lambda{socket, close, ec};
+
+    for(;;)
+    {
+        // Read a request
+        http::request<http::string_body> req;
+        http::read(socket, buffer, req, ec);
+        if(ec == http::error::end_of_stream)
+            break;
+        if(ec)
+            return fail(ec, "read");
+
+        // Send the response
+        handle_request(task, std::move(req), lambda);
+        if(ec)
+            return fail(ec, "write");
+        if(close)
+        {
+            // This means we should close the connection, usually because
+            // the response indicated the "Connection: close" semantic.
+            break;
+        }
+    }
+
+    // Send a TCP shutdown
+    socket.shutdown(tcp::socket::shutdown_send, ec);
+
+    // At this point the connection is closed gracefully
+}
+
+//------------------------------------------------------------------------------
+
+int main(int argc, char* argv[])
+{
+    try
+    {
+        // Share the options between threads.
+        auto options = parseOptions(argc, argv, cli::mode::server, true);
+
+        auto const address = net::ip::make_address("0.0.0.0");
+        auto const port = (short unsigned)options->get<size_t>("port", 5001);
+
+        // The io_context is required for all I/O
+        net::io_context ioc{1};
+
+        // The acceptor receives incoming connections
+        tcp::acceptor acceptor{ioc, {address, port}};
+        for(;;)
+        {
+            // This will receive the new connection
+            tcp::socket socket{ioc};
+
+            // Block until we get a connection
+            acceptor.accept(socket);
+
+            // Launch the session, transferring ownership of the socket
+            std::thread{std::bind(
+                &do_session,
+                std::move(socket),
+                options)}.detach();
+        }
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
 }
